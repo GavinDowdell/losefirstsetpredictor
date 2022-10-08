be1df38026f535ca9f7897b73889d0f3b613f125
GavinDowdell <50035210+GavinDowdell@users.noreply.github.com>
2022-10-08 19:45:23 +1100

    t

--- lose_first_set_predict_web_app.py
+++ lose_first_set_predict_web_app.py
@@ -28,8 +28,8 @@ The model outputs both the probability and the inverse of the probability
 '''
 app = Dash()
 server = app.server
-model = load_model('.\weights.hdf5')
-logreg_model = pickle.load(open('.\logreg_model.sav','rb'))
+model = load_model('weights.hdf5')
+logreg_model = pickle.load(open('logreg_model.sav','rb'))
 available_points_diff_indicators = list(np.linspace(-15,4,num=20))
 available_games_for_set1_indicators = [0,1,2,3,4,5,6]
 available_breaks_for_set1_indicators = [0,1,2,3,4]
